e7ccd3c89b7016ff3b5e0b1f1825d242087678a4
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-22 01:43:01 +0600

    Reusable form using props and children

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
 import './App.css';
-import HookForm from './components/HookForm/HookForm';
+import ReusableForm from './components/ReusableForm/ReusableForm';
+// import HookForm from './components/HookForm/HookForm';
 // import RefForm from './components/RefForm/RefForm';
 // import SimpleForm from './components/SimpleForm/SimpleForm';
 // import StatefulForm from './components/StatefulForm/StatefulForm';
 
 function App() {
+  const handleSingUpSubmit = (data) => {
+    console.log('Sign up data', data);
+  };
+
+  const handleUpdateProfile = (data) => {
+    console.log('handle profile data', data);
+  };
   return (
     <>
       <h1>React + Vite</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm formTitle={'Sign Up'} handleSubmit={handleSingUpSubmit}>
+        <div>
+          <h2>Sign UP</h2>
+          <p>please sign up Right now</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm
+        formTitle={'Profile Update'}
+        submitButtonTitle={'Update'}
+        handleSubmit={handleUpdateProfile}
+      >
+        <div>
+          <h2>Update Profile</h2>
+          <p>Always keep your profile Updated</p>
+        </div>
+      </ReusableForm>
     </>
   );
 }
