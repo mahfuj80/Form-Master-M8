c6ae97e1bca4cffc084f4c3fd21cb50ccd9c4f0e
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-22 00:00:07 +0600

    Form with state to get value and set default value

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
-import SimpleForm from './components/SimpleForm/SimpleForm';
+// import SimpleForm from './components/SimpleForm/SimpleForm';
+import StatefulForm from './components/StatefulForm/StatefulForm';
 
 function App() {
   return (
     <>
       <h1>React + Vite</h1>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
+      <StatefulForm></StatefulForm>
     </>
   );
 }
